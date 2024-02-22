6dec1dde8938c429326da2de12d787bc64c98f44
vaibhav-kp-github <guptavaibhav14092001@gmail.com>
2024-02-22 12:00:34 +0530

    change the path of anchor tag

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+  content: ["./dist/*.html",
+            "./*.html"],
   theme: {
     extend: {},
   },
